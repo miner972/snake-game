212f10091478c0bc7a8c355be271b51b7450933e
miner972 <badder24@gmail.com>
2024-11-05 00:29:36 +0000

    added window display in snake game

---
+++ snake-game.py
@@ -0,0 +1,7 @@
+import pygame
+
+pygame.init()
+window_width = 100
+window_height = 400
+window_display = pygame.display.set_mode((window_width,window_height))
+pygame.display.set_caption("Slithering Snakes!")
